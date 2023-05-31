9676bb2840ca454bb4e7e2f4a4a5532782472f77
JamieOnSootMC <jamie@sootmc.net>
2023-05-31 22:50:35 +0100

    remove database

--- src/main/java/uk/jamieisgeek/sootlib/SootLib.java
+++ src/main/java/uk/jamieisgeek/sootlib/SootLib.java
@@ -2,7 +2,6 @@ package uk.jamieisgeek.sootlib;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import uk.jamieisgeek.sootlib.Misc.TextManager;
-import uk.jamieisgeek.sootlib.Storage.Database;
 
 public final class SootLib {
     public final JavaPlugin provider;
@@ -16,20 +15,6 @@ public final class SootLib {
         this.provider = provider;
         this.textManager = new TextManager();
         instance = this;
-    }
-
-    /**
-     * @param address The address of the database
-     * @param database The name of the database
-     * @param username The username for the database
-     * @param password The password for the database
-     * @param port The port for the database
-     * @return The database
-     * @throws ClassNotFoundException If the database driver is not found
-     */
-    public Database createDatabase(String address, String database, String username, String password, String port) throws ClassNotFoundException {
-        Class.forName("org.mariadb.jdbc.Driver");
-        return new Database(address, database, username, password, port);
     }
 
     /**
